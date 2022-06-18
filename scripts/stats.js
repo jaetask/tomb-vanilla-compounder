103be35ce2116da7b1b2e8d0bcf3f8d584fa53f5
jaetask <me@jaetask.com>
2022-06-18 21:21:55 +0100

    add stats script

---
+++ scripts/stats.js
@@ -0,0 +1,62 @@
+const LqdrAddress = "0x10b620b2dbac4faa7d7ffd71da486f5d44cd86f9";
+const SpiritSwapLqdrFtmPoolId = 0;
+const SpiritSwapLqdrFtmLPAddress = "0x4fe6f19031239f105f753d1df8a0d24857d0caa2";
+const FarmProxyAddress = "0x6e2ad6527901c9664f016466b8DA1357a004db0f";
+// const FarmRealAddress = "0x5CEE2988184afE3CD807e0178B394259E8cdC56C"; This is the MasterChefV2
+const SpiritRouterAddress = "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52";
+const OperatorAddress = "0x44b4d3Cb8087030A83B07ba2E2803e6D313Cf845";
+const DeployedContract = "0x1a0cb0ecd122707d7e3c543af0025371e81f8b24";
+
+const DEV_ADDRESS = process.env.DEV_ADDRESS;
+const DEPLOYED_CONTRACT = process.env.DEPLOYED_CONTRACT_V1;
+import { formatUnits } from "ethers/utils";
+
+/**
+ * COMMAND LINE
+ * npx hardhat run scripts/stats.js --network mainnet
+ *
+ * UNISWAP DOCS
+ * https://docs.uniswap.org/protocol/V2/reference/smart-contracts/pair
+ */
+async function main() {
+  const [depositor] = await ethers.getSigners();
+  console.log("Using wallet", depositor.address);
+
+  //------------------------------------------------------
+  // we need to approve the deployed contract to spend the LP tokens
+  const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
+  const spiritSwapLP = UniswapV2Pair.attach(SpiritSwapLqdrFtmLPAddress);
+  const LiquidDriverSoloCrypt = await ethers.getContractFactory("LiquidDriverSoloCrypt");
+  const liquidDriverSoloCrypt = await LiquidDriverSoloCrypt.attach(DEPLOYED_CONTRACT);
+
+  // does the crypt have any allowance?
+  const preAllowance = await spiritSwapLP.allowance(depositor.address, DEPLOYED_CONTRACT);
+  if (preAllowance.gt(0)) {
+    console.log("Crypt has allowance of", preAllowance.toString());
+  } else {
+    console.log("Crypt out of allowance");
+  }
+
+  // how much LP is in the wallet?
+  const lpInTheWallet = await spiritSwapLP.balanceOf(depositor.address);
+  console.log("lpInTheWallet", formatUnits(lpInTheWallet));
+
+  // how much LP is in the crypt?
+  const lpInTheCrypt = await spiritSwapLP.balanceOf(DEPLOYED_CONTRACT);
+  console.log("lpInTheCrypt", formatUnits(lpInTheCrypt));
+
+  // how much LP has the contract staked in the farm?
+  const lpInTheFarm = await liquidDriverSoloCrypt.getLPBalanceAtFarm();
+  console.log("lpInTheFarm", formatUnits(lpInTheFarm));
+
+  // how many pending LQDR rewards are there?
+
+  // how much LQDR is in the contract
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
